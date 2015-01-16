e8309e36e644f1caa3260d631c87de8ca1549952
ricky.zhiyang@gmail.com <ricky.zhiyang@gmail.com>
2015-01-16 18:33:33 +0800

    add new function for tree

--- decision_tree/trees.py
+++ decision_tree/trees.py
@@ -1,5 +1,6 @@
 #encoding=utf-8
 from math import log
+from draw_tree import *
 '''
     calculate Shannon Entropy of the dataSet
 '''
@@ -61,7 +62,22 @@ def genDataSet():
                 [0,1,'no'],
                 [0,1,'no']]
     labels = ['no surfacing','flippers']
-
+    dataSet = [
+            ['Sunny','Hot','High','Weak','No'],
+            ['Sunny','Hot','High','Strong','No'],
+            ['Overcast','Hot','High','Weak','Yes'],
+            ['Rain','Mild','High','Weak','Yes'],
+            ['Rain','Cool','Normal','Weak','Yes'],
+            ['Rain','Cool','Normal','Strong','No'],
+            ['Overcast','Cool','Normal','Strong','Yes'],
+            ['Sunny','Mild','High','Weak','No'],
+            ['Sunny','Cool','Normal','Weak','Yes'],
+            ['Rain','Mild','Normal','Weak','Yes'],
+            ['Sunny','Mild','Normal','Strong','Yes'],
+            ['Overcast','Mild','High','Strong','Yes'],
+            ['Overcast','Hot','Normal','Weak','Yes'],
+            ['Rain','Mild','High','Strong','No']]
+    labels = ['Outlook','Temperature','Humidity','Wind']
     return dataSet,labels
 
 '''
@@ -96,6 +112,33 @@ def createTree(dataSet, labels):
         subSet = splitDataSet(dataSet,bestFeat,value)
         myTree[bestFeatLabel][value] = createTree(subSet,subLabels)
     return myTree
+'''
+    classify
+'''
+def classify(dTree, labels, featureVec):
+    firstLabel = dTree.keys()[0]
+    
+    firstIndex = labels.index(firstLabel)
+    secondDic = dTree[firstLabel]
+    for key in secondDic:
+        if key == featureVec[firstIndex]:
+            if type(secondDic[key]).__name__ == 'dict':
+                return classify(secondDic[key],labels,featureVec)
+            else:
+                return secondDic[key]
+
+def storeTree(dTree, filename):
+    import pickle
+    fw = open(filename,'w')
+    pickle.dump(dTree,fw)
+    fw.close()
+
+def loadTree(filename):
+    import pickle
+    fr = open(filename)
+    dTree = pickle.load(fr)
+    return dTree
+
 
 def testCalcShannonEnt():
     dataSet,labels = genDataSet()
@@ -104,7 +147,14 @@ def testCalcShannonEnt():
 
 if __name__ == '__main__':
     dataSet,labels = genDataSet()
-    print createTree(dataSet,labels)
+    
+    myTree = createTree(dataSet,labels[:])
+    print labels
+    classify(myTree,labels,['Rain','Cool','Normal','Weak'])
+    storeTree(myTree,'testFile.tree')
+    dTree = loadTree('testFile.tree')
+    print classify(dTree,labels,['Rain','Cool','Normal','Weak'])
+    #createPlot(myTree)
 
 
 
